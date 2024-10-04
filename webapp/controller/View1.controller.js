4b2d6ad8316343eb4dfb29f5eef6e4786e535fd8
juan arias <juangabrielarias22@gmail.com>
2024-10-04 20:23:44 +0000

    se agrego filtro en vista principal

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -7,6 +7,46 @@ function (Controller) {
     return Controller.extend("academia.vuelos.controller.View1", {
         onInit: function () {
 
+        },
+
+        onAfterRendering: function() {
+            let aAerolineas = [{aerolinea:'AA'},
+                {aerolinea:'FLY'},
+                {aerolinea:'JET'},
+            ];
+            const oModelAerolineas = new sap.ui.model.json.JSONModel(aAerolineas);
+            this.getView().setModel(oModelAerolineas, "oModelAerolineas");
+
+            const aModelVuelos = new sap.ui.model.json.JSONModel(aAerolineas);
+            this.getView().setModel(aModelVuelos, "aModelVuelos");
+            let oFilter = []
+            this._getDataVuelos(oFilter)
+        },
+
+        onSearch: function(oEvent) {
+            let idAerolinea = this.byId('idAerolinea').getSelectedKey()
+            let oFilter = [] 
+            oFilter.push(new sap.ui.model.Filter(
+                            "Aerolinea",
+                            sap.ui.model.FilterOperator.EQ,
+                            idAerolinea
+                          ));
+            this._getDataVuelos(oFilter)
+        },
+
+        _getDataVuelos: function(oFilter) {
+
+            let oModel = this.getView().getModel() 
+                oModel.read("/vueloSet", {
+                    filters:oFilter,
+                    success: function (oData) {
+                        this.getView().getModel('aModelVuelos').setData(oData.results)
+                    }.bind(this),
+                    error: function () {
+                      sap.m.MessageToast.show("Error al conectar con SAP");
+                    }.bind(this),
+                  });
         }
+
     });
 });
